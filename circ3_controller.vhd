99e3f08b39c272480d1d66f7a1ddc32aa1dda9f6
Alisson Boeing <alissonmboeing@gmail.com>
2023-10-16 06:27:10 -0300

    Add circ3 files

---
+++ circ3_controller.vhd
@@ -0,0 +1,50 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity circ3_controller is
+  port (
+     i_A : in std_logic; 							   -- input 
+     i_S : in std_logic_vector(2 downto 0);     -- Current state               
+	  o_R: out std_logic;								-- Output
+     o_N   : out std_logic_vector(2 downto 0)); -- netxt state
+end circ3_controller;
+
+architecture arch_1 of circ3_controller is
+  signal w_NEXT  : std_logic_vector(2 downto 0); -- next state
+  signal r_STATE : std_logic_vector(2 downto 0); -- current state
+  signal w_R : std_logic;
+begin
+
+	r_STATE <= i_S;
+	
+  -- Next State Function
+  process (w_NEXT, r_State, i_A)
+  begin
+    case r_STATE is
+      when "000" => if (i_A = '0') then
+        w_NEXT <= "000";
+      else
+        w_NEXT <= "001";
+      end if;
+      when "001"   => w_NEXT   <= "010";
+      when "010"   => w_NEXT   <= "011";
+      when "011"   => w_NEXT   <= "100";
+      when "100"   => w_NEXT   <= "000";
+      when others => w_NEXT <= "000";
+    end case;
+  end process;
+  
+	process ( w_R, r_STATE)
+	begin
+	if r_STATE = "001" or r_STATE = "010" or r_STATE = "100" then
+		w_R <= '1' ;
+	else
+		w_R <= '0';
+	end if;
+	end process;
+  
+  -- Output Function
+  o_R <= w_R;
+  o_N <= w_Next;
+  
+end arch_1;
---
+++ circ3_toplevel.vhd
@@ -0,0 +1,47 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity circ3_toplevel is
+   port (
+      i_CLK : in std_logic;
+      i_RST : in std_logic;
+      i_A : in std_logic;
+      o_R : out std_logic
+   );
+end entity;
+
+architecture rtl of circ3_toplevel is
+   signal w_n_S     : std_logic_vector(2 downto 0) := "000";
+	signal w_c_S     : std_logic_vector(2 downto 0) := "000";
+   component circ3_controller is
+      port (
+     i_A : in std_logic; 							   -- input 
+     i_S : in std_logic_vector(2 downto 0);     -- Current state               
+	  o_R: out std_logic;								-- Output
+     o_N   : out std_logic_vector(2 downto 0)); -- netxt state
+   end component;
+   component circ3_state_register is
+      port (
+     i_CLK : in std_logic;                      -- clock
+     i_RST : in std_logic;                      -- reset/clear
+	  i_N	  : in std_logic_vector(2 downto 0);   -- next state
+     o_S   : out std_logic_vector(2 downto 0) -- current state
+	  );
+   end component;
+begin
+
+   u_Controller : circ3_controller
+   port map(
+      i_A => i_A,
+		i_S => w_c_S,
+		O_N => w_n_S,
+      o_R  => o_R
+   );
+	
+   u_Register : circ3_state_register port map(
+      i_CLK => i_CLK,
+      i_RST => i_RST,
+      i_N   => w_n_S,
+		o_S   => w_c_S
+   );
+end architecture;
